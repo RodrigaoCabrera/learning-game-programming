57e24b39fd0b5f2f0121c2c50c8af3b0cac351ae
Rodrigo Cabrera <rodrigod33d@gmail.com>
2024-01-12 14:35:51 -0300

    ADD: dog animation

---
+++ index.js
@@ -0,0 +1,100 @@
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", (e) => {
+  console.log("test");
+  playerState = e.target.value;
+});
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+const spriteWidth = 575;
+const spriteHeight = 523;
+let playerState = "run";
+let gameFrame = 0;
+let staggerFramer = 5;
+const spriteAnimations = [];
+const animationSattes = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 8,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 7,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+animationSattes.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  //ctx.fillRect(gameFrame, 50, 200, 100);
+  let position =
+    Math.floor(gameFrame / staggerFramer) %
+    spriteAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
+  if (gameFrame % staggerFramer === 0) {
+    if (frameX < 6) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
+
+  requestAnimationFrame(animate);
+}
+animate();
